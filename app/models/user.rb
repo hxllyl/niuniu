e86614a3eece595afe048e538a6fd68563c6c03c
张春明Evan.Zhang <evan.zhang@edoctor.cn>
2015-03-10 16:19:56 +0800

    update User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,32 @@
 # encoding: utf-8
+
 # author: depp.yu
 
 class User < ActiveRecord::Base
+
+  # validates
+
+  # tables relation
+
+  # constants
+
+  Roles = %w(normal admin)
+
+  Status = {
+    0  => 'unapproved',
+    1  => 'approved',
+    -1 => 'deleted'
+  }
+
+  Levels = {
+    0 => '个人',
+    1 => '认证资源',
+    2 => '认证综展',
+    3 => '4S'
+  }
+
+  # class methods
+
+  # instance methods
 
 end
