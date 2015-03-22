52edb43d8bc93d8c3812e5e2f8449e10cdf01057
depp.yu <depp.yu@lankr.cn>
2015-03-22 19:06:09 +0800

    asset production load

--- config/application.rb
+++ config/application.rb
@@ -46,7 +46,7 @@ module NiuNiu
     config.assets.precompile += [Proc.new { |path| File.basename(path) =~ /^[^_][a-z0-9-]+\.css$/ }]
 
     config.assets.precompile << Proc.new do |path|
-       if path =~ /\.(js)\z/
+       if path =~ /\.(css|js|scss|png|jpg|gif|json)\z/
          full_path = Rails.application.assets.resolve(path).to_path
         app_assets_path = Rails.root.join('app', 'assets').to_path
         if full_path.starts_with? app_assets_path
