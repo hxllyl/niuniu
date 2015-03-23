f6d377177bc7e6871d342db22a8b133c30f51b0c
depp.yu <depp.yu@lankr.cn>
2015-03-23 17:21:41 +0800

    validate validCode api

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -51,7 +51,7 @@ class Users::SessionsController < Devise::SessionsController
   
   # configure redirect to viewer after devise destory
   def after_sign_out_path_for(resource_or_scope)
-    request.referrer || '/'
+    '/'
   end
    
 end
