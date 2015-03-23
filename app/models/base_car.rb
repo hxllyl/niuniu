677ae288eb42e473795a2a9f94d48a541eb13840
evan.zhang <evan.zhang@edoctor.cn>
2015-03-23 12:03:28 +0800

    fix api base_cars

--- app/models/base_car.rb
+++ app/models/base_car.rb
@@ -3,20 +3,33 @@
 # author: depp.yu
 
 class BaseCar < ActiveRecord::Base
-  
+
   STATUS = {
     0 => '激活',
     1 => '未激活'
   }
-  
+
   belongs_to :standard, class_name: 'Standard' # 属于那种规格
   belongs_to :brand, class_name: 'Brand' # 属于那种车辆品牌
   has_many   :car_photos, as: :owner, dependent: :nullify, autosave: true # 图片数据
-  
-  
+
+
   # instance_methods
   def to_human_name
-    self.brand.name << self.model << self.NO  
+    self.brand.name << self.model << self.NO
   end
-  
+
+  def to_hash
+    {
+      id:             id,
+      resource_name:  'BaseCar',
+      model:          model,
+      style:          style,
+      number:         self.NO,
+      outer_color:    outer_color,
+      inner_color:    inner_color,
+      base_price:     base_price.to_f
+    }
+  end
+
 end
