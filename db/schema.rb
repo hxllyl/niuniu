ec69f8444f63862aae1cf5d594be694018b9b387
depp.yu <depp.yu@lankr.cn>
2015-03-24 13:05:33 +0800

    custom center

--- db/schema.rb
+++ db/schema.rb
@@ -121,8 +121,8 @@ ActiveRecord::Schema.define(version: 20150313092115) do
     t.integer  "standard_id"
     t.string   "model",             limit: 40,                                        null: false
     t.string   "style",             limit: 60
-    t.string   "outer_color",       limit: 20,                                        null: false
-    t.string   "inner_color",       limit: 20,                                        null: false
+    t.string   "outer_color",       limit: 60,                                        null: false
+    t.string   "inner_color",       limit: 60,                                        null: false
     t.string   "car_license_areas", limit: 60,                                        null: false
     t.string   "car_in_areas",                                           default: [], null: false, array: true
     t.integer  "take_car_date",                                          default: 0
