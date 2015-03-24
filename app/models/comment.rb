992c22982b4b094bd9c171f4013bdec9f9d2e804
leslie.wen <leslie.wen@edoctor.cn>
2015-03-24 17:56:58 +0800

    update comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -7,8 +7,8 @@ class Comment < ActiveRecord::Base
   belongs_to :resource, polymorphic: true
 
   belongs_to :parent,  class_name: 'Comment'
-  has_many :sub_comments, :foreign_key => :parent_id, class_name: 'Comment'
+  has_many :sub_comments, foreign_key: :parent_id, class_name: 'Comment'
 
 
-  validates_inclusion_of :resource_type,  { :in => %w[ Comment Post  ] }
+  validates_inclusion_of :resource_type,   in: %w[ Comment Post  ]
 end
