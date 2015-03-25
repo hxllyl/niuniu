589180c644ad6c7878c04a6b529bb39d1cd8fd17
depp.yu <depp.yu@lankr.cn>
2015-03-25 21:53:03 +0800

    update

--- app/helpers/portal_helper.rb
+++ app/helpers/portal_helper.rb
@@ -16,7 +16,7 @@ module PortalHelper
 
   # post在首页的名称显示
   def post_name(post)
-    t('find') << post.base_car.to_human_name << ' ' << t('sale') << post.car_license_areas
+    t('find') << post.base_car_to_human_name << ' ' << t('sale') << post.car_license_areas
   end
 
 end
