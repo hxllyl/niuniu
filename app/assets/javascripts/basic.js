ed69de9914d38656c2c07c09310179cdd6dad233
行翀 <xingchong2007@163.com>
2015-03-26 10:59:45 +0800

    placeholder js

--- app/assets/javascripts/basic.js
+++ app/assets/javascripts/basic.js
@@ -3,7 +3,40 @@ $(document).ready(function(){
     e.preventDefault();
     $(this).tab("show");
   });
+  /*翻页居中*/
   $(".pageList").css("margin-left", -$(".pageList").outerWidth(true)/2 + "px");
+  /*placeholder兼容*/
+  var doc = document,
+  inputs = doc.getElementsByTagName("input"),
+  supportPlaceholder = "placeholder" in doc.createElement("input"),
+  placeholder = function(input) {
+    var text = input.getAttribute("placeholder"),
+    defaultValue = input.defaultValue;
+    if (defaultValue == "") {
+      input.value = text
+    }
+    input.onfocus = function() {
+      if (input.value === text) {
+        this.value = ""
+      }
+    };
+    input.onblur = function() {
+      if (input.value === "") {
+        this.value = text
+      }
+    }
+  };
+  if (!supportPlaceholder) {
+    for (var i = 0,
+    len = inputs.length; i < len; i++) {
+      var input = inputs[i],
+      text = input.getAttribute("placeholder");
+      if (input.type === "text" && text) {
+        placeholder(input)
+      }
+    }
+  }
+  /*nav fixed*/
   $(window).scroll(function(){
     var scrollTop = $(this).scrollTop();
     if(scrollTop > 0){
