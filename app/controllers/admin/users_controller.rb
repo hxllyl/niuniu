8f6e17765258c37996b61ea0e1ab5d8139da8798
luoping <luoping0425@gmail.com>
2015-03-31 08:09:32 +0800

    手机号码查询，请添加搜索后逻辑

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,6 +1,16 @@
 class Admin::UsersController < Admin::BaseController
 
   def search
-    
+    if request.xhr?
+      @mobile = params[:mobile]
+      if @mobile.present?
+        @user = User.where(mobile: @mobile).first
+      end
+    end
+
+    respond_to do |format|
+      format.html
+      format.js { render :layout => false }
+    end
   end
 end
