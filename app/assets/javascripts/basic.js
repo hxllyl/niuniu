45f254d8930048327578664618e1286b541837fb
行翀 <xingchong2007@163.com>
2015-04-04 15:57:06 +0800

    fix js

--- app/assets/javascripts/basic.js
+++ app/assets/javascripts/basic.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  //$(".dropdown-toggle").dropdown();
+  $(".dropdown-toggle").dropdown();
   $(".nav-tabs a").click(function (e) {
     e.preventDefault();
     $(this).tab("show");
