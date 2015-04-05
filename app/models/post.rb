9406dfee7cf10949cb0e23371d8b31a1ccfa0bae
leslie.wen <leslie.wen@edoctor.cn>
2015-04-05 14:19:19 +0800

    comemnt post solr search

--- app/models/post.rb
+++ app/models/post.rb
@@ -59,29 +59,29 @@ class Post < ActiveRecord::Base
     expect_price: '价格排序'
   }
 
-  searchable do
-    text :inner_color
-    text :outer_color
-    text :brand do
-      brand.name
-    end
-
-    text :standard do
-      standard.name
-    end
-
-    text :car_model do
-      car_model.name
-    end
-
-    text :base_car do
-      base_car.style
-    end
-    text :title
-    integer :_type
-    integer :user_id
-
-  end
+  # searchable do
+  #   text :inner_color
+  #   text :outer_color
+  #   text :brand do
+  #     brand.name
+  #   end
+  #
+  #   text :standard do
+  #     standard.name
+  #   end
+  #
+  #   text :car_model do
+  #     car_model.name
+  #   end
+  #
+  #   text :base_car do
+  #     base_car.style
+  #   end
+  #   text :title
+  #   integer :_type
+  #   integer :user_id
+  #
+  # end
 
   # relations
   has_many    :tenders, class_name: 'Tender'
