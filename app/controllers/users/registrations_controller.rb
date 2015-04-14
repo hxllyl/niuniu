120a906e35715cba39c72617011bd21126b47fbf
depp.yu <depp.yu@lankr.cn>
2015-04-14 15:54:13 +0800

    bug fix

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 class Users::RegistrationsController < Devise::RegistrationsController
   before_filter :configure_sign_up_params, only: [:create]
   before_filter :configure_account_update_params, only: [:update]
