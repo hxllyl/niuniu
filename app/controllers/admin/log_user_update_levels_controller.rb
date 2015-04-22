e3745742c2da4d824d6681bfabd1b8861b00b547
evan.zhang <evan.zhang@edoctor.cn>
2015-04-22 18:09:33 +0800

    fix admin user_

--- app/controllers/admin/log_user_update_levels_controller.rb
+++ app/controllers/admin/log_user_update_levels_controller.rb
@@ -1,14 +1,14 @@
 # encoding: utf-8
 
 class Admin::LogUserUpdateLevelsController < Admin::BaseController
-  
+
   def index
-    @logs = Log::UserUpdateLevel.order(status: :asc, updated_at: :desc).page(params[:page] || 1).per(20)
+    @logs = Log::UserUpdateLevel.unapproved.order(status: :asc, updated_at: :desc).page(params[:page] || 1).per(20)
   end
-  
+
   def show
     @log = Log::UserUpdateLevel.find params[:id]
-    @types = case @log.end_level 
+    @types = case @log.end_level
            when 1 then
             %w(identity hand_id)
            when 2 then
@@ -19,10 +19,10 @@ class Admin::LogUserUpdateLevelsController < Admin::BaseController
             %w(visiting room_outer room_inner)
             end
   end
-  
+
   def update_status
     @log = Log::UserUpdateLevel.find params[:id]
-    
+
     @log.transaction do
       if @log.update(status: params[:status].to_i)
         @log.reload
@@ -30,16 +30,16 @@ class Admin::LogUserUpdateLevelsController < Admin::BaseController
                   @log.end_level.to_i
                 else
                   @log.start_level.to_i
-                end        
-        @log.user.update(level: level)        
+                end
+        @log.user.update(level: level)
         # TODO: 加操作人
         flash[:notice] = '操作成功'
       else
         flash[:error]  = '操作不成功'
       end
     end
-    
+
     redirect_to :back
   end
-  
+
 end
--- app/models/log/user_update_level.rb
+++ app/models/log/user_update_level.rb
@@ -1,19 +1,21 @@
 # encoding: utf-8
 
 class Log::UserUpdateLevel < ActiveRecord::Base
-  
+
   STATUS = {
     0 => '待审核',
     1 => '审核通过',
     2 => '审核不通过'
   }
-  
+
   belongs_to :user, class_name: 'User'
   belongs_to :operator, class_name: 'Staff'
-  
+
   delegate :name, to: :user, prefix: true, allow_nil: true
   delegate :name, to: :operator, prefix: true, allow_nil: true
-  
+
+  scope :unapproved, -> { where('status <> 1') }
+
   def show_photos_with_level
     types = case end_level
             when User::LEVELS.keys[1] then
@@ -22,24 +24,24 @@ class Log::UserUpdateLevel < ActiveRecord::Base
               ['license']
             when User::LEVELS.keys[3] then
               ['visiting', 'room_outer', 'room_inner', 'license']
-            else 
+            else
               ['visiting', 'room_outer', 'room_inner']
             end
-            
-    photos = users.photos.where('_type in (?)', types)        
+
+    photos = users.photos.where('_type in (?)', types)
   end
-  
+
   before_create :has_same_request?
-  
+
   def has_same_request?
     log = Log::UserUpdateLevel.find_by(user: user, start_level: start_level, end_level: end_level, status: STATUS.keys[0])
     return if log
   end
-  
+
   def is_wait?
     status == STATUS.keys[0]
   end
-  
+
   def is_approved?
     status == STATUS.keys[1]
   end
